0ff358ecb35f401198d2a3f5b6947ee829ee4001
fadna Edan <fadnaaden05@gmail.com>
2025-07-25 20:33:52 +0300

    updates

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,19 +1,20 @@
 const jwt = require("jsonwebtoken");
 const User = require("../models/User");
 
-module.exports = async (req, res, next) => {
+const jwt = require('jsonwebtoken');
+module.exports = (req, res, next) => {
   const authHeader = req.headers.authorization;
-  if (!authHeader || !authHeader.startsWith("Bearer ")) {
-    return res.status(401).json({ message: "Token ma jiro ama waa khaldan" });
+  if (!authHeader || !authHeader.startsWith('Bearer ')) {
+    return res.status(401).json({ message: 'Token ma jiro ama waa khaldan' });
   }
 
-  const token = authHeader.split(" ")[1];
-
+  const token = authHeader.split(' ')[1];
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = decoded.user || decoded;
+    req.user = decoded;
     next();
   } catch (err) {
-    return res.status(401).json({ message: "Token invalid ama wuu dhacay" });
+    return res.status(401).json({ message: 'Token invalid ama wuu dhacay' });
   }
 };
+
