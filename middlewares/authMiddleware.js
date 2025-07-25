c0ffb1d66d08532d64c8dde0f47ec9163e7397b0
fadna Edan <fadnaaden05@gmail.com>
2025-07-25 16:36:01 +0300

    updates

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,27 +1,19 @@
-// middlewares/authMiddleware.js
+// authMiddleware.js
 
-const jwt = require("jsonwebtoken");
+const jwt = require("jsonwebtoken")
+const  authMiddleware = (req, res, next) => {
+   const token = req.headers.authorization?.split(" ")[1];
 
-const authMiddleware = (req, res, next) => {
-  const authHeader = req.headers.authorization;
+   if(!token) {
+       res.status(401).json({message: "Access denied"})
+   }
 
-  // Hubi in token la diray
-  if (!authHeader || !authHeader.startsWith("Bearer ")) {
-    return res.status(401).json({ message: "Access denied: Token ma jiro ama waa khaldan yahay" });
-  }
+   const decoded = jwt.verify(token, process.env.JWT_SECRET)
 
-  const token = authHeader.split(" ")[1];
+   req.user = decoded?.user;
 
-  try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+   next();
 
-    // Ku dar user info request-ka
-    req.user = decoded?.user || decoded; // haddii aad encode-gareyso { user: { id, email, role } }
+}
 
-    next(); // u gudub route-ga
-  } catch (error) {
-    return res.status(401).json({ message: "Token invalid ama wuu dhacay" });
-  }
-};
-
-module.exports = authMiddleware;
+module.exports = authMiddleware
